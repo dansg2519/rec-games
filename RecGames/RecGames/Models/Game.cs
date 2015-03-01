61fe6a06dc90a6d9a0782193344ed1885569206e
dansg2519 <daniel.dsouzag@gmail.com>
2015-03-01 20:26:19 -0300

    Projeto criado e classe modelo Game iniciada.

---
+++ RecGames/RecGames/Models/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RecGames.Models
+{
+    public class Game
+    {
+        [Key]
+        public int GameID { get; set; }
+        public string Name { get; set; }
+        public string ControllerSupport { get; set; }
+        public string Platforms { get; set; }
+        public string Developers { get; set; }
+        public string Publishers { get; set; }
+        public int Recommendations { get; set; }
+        public int MetacriticScore { get; set; }
+        public int PriceID { get; set; }
+    }
+}
---
+++ RecGames/RecGames/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecGames.Startup))]
+namespace RecGames
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
