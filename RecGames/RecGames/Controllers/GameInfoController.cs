d9294898fe06ad974107e1b4381bab51b3a17fc6
cyro809 <cyro809@gmail.com>
2015-06-06 02:35:40 -0300

    Rota para pegar as tags do jogo usando id

--- RecGames/RecGames/Controllers/GameInfoController.cs
+++ RecGames/RecGames/Controllers/GameInfoController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
 
 namespace RecGames.Controllers
 {
@@ -11,6 +13,7 @@ namespace RecGames.Controllers
     {
 
         private const string SteamKey = "3E2BA9478DC190757ABE4D1DABEA9802";
+        HtmlDocument htmlDocument = new HtmlDocument();
 
 
         // GET api/GameInfo
@@ -24,6 +27,37 @@ namespace RecGames.Controllers
             }
 
             return this.Ok(gameInfoJson);
+        }
+
+
+        [HttpGet]
+        public IHttpActionResult Tags(int? id)
+        {
+            List<string> tags = new List<string>();
+            string title;
+
+            using (WebClient client = new WebClient())
+            {
+                string html = client.DownloadString("http://store.steampowered.com/app/" + id);
+                htmlDocument.LoadHtml(html);
+
+            }
+
+            HtmlNode htmlNode = htmlDocument.DocumentNode.SelectSingleNode("//*[@id='game_highlights']/div[2]/div/div[5]/div[2]");
+            
+            try
+            {
+                string temp = htmlNode.InnerText;
+
+                title = Regex.Replace(temp, "\t", "");
+                title = Regex.Replace(title, @"( |\r?\n)\1+", "$1");
+                tags = title.Split('\n').ToList();
+                return this.Ok(tags);
+            }
+            catch (System.NullReferenceException)
+            {
+                return this.Ok("No Tags");
+            }
         }
     }
 }
