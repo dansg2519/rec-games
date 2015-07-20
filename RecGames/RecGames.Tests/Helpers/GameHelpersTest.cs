36da16a787c63e625df0788a23cc97aecea99d8d
cyro809 <cyro809@gmail.com>
2015-07-20 19:38:50 -0300

    Remove HomeControllerTest. Adiciona testes para método SupportedPlatforms da classe GameHelpers.

---
+++ RecGames/RecGames.Tests/Helpers/GameHelpersTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using RecGames.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace RecGames.Tests.Helpers
+{
+    [TestClass]
+    public class GameHelpersTest
+    {
+        [TestMethod]
+        public void TestSupportedPlatformsMustReturnWindowsIfWindowsIsSupported()
+        {
+            string jsonText = @"
+            {
+                windows: true,
+                mac: false,
+                linux: false
+            }";
+            var platforms = JObject.Parse(jsonText);
+
+            Assert.AreEqual(GameHelpers.SupportedPlatforms(platforms), "Windows ");
+            
+        }
+
+        [TestMethod]
+        public void TestSupportedPlatformsMustReturnMacIfMacIsSupported()
+        {
+            string jsonText = @"
+            {
+                windows: false,
+                mac: true,
+                linux: false
+            }";
+            var platforms = JObject.Parse(jsonText);
+
+            Assert.AreEqual(GameHelpers.SupportedPlatforms(platforms), "Mac ");
+        }
+
+        [TestMethod]
+        public void TestSupportedPlatformsMustReturnLinuxIfLinuxIsSupported()
+        {
+            string jsonText = @"
+            {
+                windows: false,
+                mac: false,
+                linux: true
+            }";
+            var platforms = JObject.Parse(jsonText);
+
+            Assert.AreEqual(GameHelpers.SupportedPlatforms(platforms), "Linux ");
+        }
+
+        [TestMethod]
+        public void TestSupportedPlatformsMustReturnEmptyStringIfNonePlatformsAreSupported()
+        {
+            string jsonText = @"
+            {
+                windows: false,
+                mac: false,
+                linux: false
+            }";
+            var platforms = JObject.Parse(jsonText);
+
+            Assert.AreEqual(GameHelpers.SupportedPlatforms(platforms), "");
+        }
+
+    }
+}
