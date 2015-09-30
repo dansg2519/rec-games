7be76d1766eab5a5f411d27bcc157dffce4d5d7f
cyro809 <cyro809@gmail.com>
2015-09-29 21:36:29 -0300

    Ajustando texto justificativo. Altera cor do player container.

--- RecGames/RecGames/Helpers/GameHelpers.cs
+++ RecGames/RecGames/Helpers/GameHelpers.cs
@@ -69,7 +69,8 @@ namespace RecGames.Helpers
                     new JProperty("justification", string.Format(Strings.Justification, g.Name, string.Join(",", g.Tags.Select(t => t.TagName).ToArray()), g.Recommendations, g.MetacriticScore)),
                     new JProperty("tags", g.Tags.Select(t => t.TagName)),
                     new JProperty("common_tags", string.Join(",", g.Tags.Select(t => t.TagName).ToArray().Intersect(playerPortrait))),
-                    new JProperty("tags_string", string.Join(",", g.Tags.Select(t => t.TagName).ToArray()))))));
+                    new JProperty("tags_string", string.Join(",", g.Tags.Select(t => t.TagName).ToArray())),
+                    new JProperty("uncommon_tags", string.Join(",", g.Tags.Select(t => t.TagName).ToArray().Except(playerPortrait)))))));
 
             return gamesToRecommendJson;
         }
